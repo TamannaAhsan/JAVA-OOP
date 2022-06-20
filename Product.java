c9920d47d8fe5a0fe3b6b06cc3f7166d5866a75f
Brishty <52675761+TamannaAhsan@users.noreply.github.com>
2022-06-20 20:51:41 +0600

    Update Product.java

--- Product.java
+++ Product.java
@@ -30,29 +30,50 @@ public class Product {
     public int getId() {
         return id;
     }
+    public void setId(int id) {
+        this.id= id;
+    }
 
     public String getName() {
         return name;
+    }
+    public void setNmane(String name) {
+        this.name= name;
     }
 
     public String getDescription() {
         return description;
     }
+    public void setDescription(String description) {
+        this.description= description;
+    }
 
     public String getCategory() {
         return category;
+    }
+    public void setCategory(String category) {
+        this.category= category;
     }
 
     public String getBrand() {
         return brand;
     }
+    public void setBrand(String brand) {
+        this.brand= brand;
+    }
 
     public double getPrice() {
         return price;
     }
+    public void setPrice(double price) {
+        this.price= price;
+    }
 
     public int getAmountofStock() {
         return amountStock;
+    }
+    public void setAmountofStock(int amountStock) {
+        this.amountStock = amountStock;
     }
 
     public String display() {
