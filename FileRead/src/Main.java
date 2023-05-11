407ae19ec5e827c73481007dd4f75a8473c02a13
Brishty <52675761+TamannaAhsan@users.noreply.github.com>
2023-05-11 16:13:33 +0600

    Add files via upload

---
+++ FileRead/src/Main.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        List<Employee> employeeList = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader("sample.txt"));
+        List<String> allLines = new ArrayList<>();
+
+        String line;
+        try{
+            while ((line = br.readLine())!=null){
+                allLines.add(line);
+            }
+            br.close();
+            allLines = allLines.stream()
+                    .skip(1)
+                    .collect(Collectors.toList());
+
+            for(String values : allLines){
+                String [] data = values.split(",");
+                Integer id = Integer.parseInt(data[0]);
+                String name = data[1];
+                Integer age = Integer.parseInt(data[2]);
+
+                employeeList.add(new Employee(id,name,age));
+            }
+
+            for(Employee emp : employeeList){
+
+                System.out.println("Id: "+emp.getId()+" Name: "+emp.getName()+" Age: "+emp.getAge());
+            }
+
+        }catch (Exception e){
+            System.out.println("Exception "+e);
+        }
+    }
+}
