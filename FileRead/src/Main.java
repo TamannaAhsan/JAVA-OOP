12a8ff427ace44afce291a79c0bbac216e7c68b7
Brishty <52675761+TamannaAhsan@users.noreply.github.com>
2023-05-11 16:22:12 +0600

    Update Main.java

--- FileRead/src/Main.java
+++ FileRead/src/Main.java
@@ -16,24 +16,29 @@ public class Main {
         String line;
         try{
             while ((line = br.readLine())!=null){
+                //store string values in list
                 allLines.add(line);
             }
             br.close();
+            
+            //remove 1st row of string ("id","name", "age")
             allLines = allLines.stream()
                     .skip(1)
                     .collect(Collectors.toList());
 
+            //spilt the lines and parse the data
             for(String values : allLines){
                 String [] data = values.split(",");
                 Integer id = Integer.parseInt(data[0]);
                 String name = data[1];
                 Integer age = Integer.parseInt(data[2]);
 
+                //Create new employee and add to the employeeList
                 employeeList.add(new Employee(id,name,age));
             }
 
+            //Print the data
             for(Employee emp : employeeList){
-
                 System.out.println("Id: "+emp.getId()+" Name: "+emp.getName()+" Age: "+emp.getAge());
             }
 
